824a10c19ac59194003c938441721f5be65be985
Unknown <nabil.ghedjati@gmail.com>
2017-05-16 01:04:18 +0200

    first commit since a very long time (big problems of the app are solved)

--- js/app.js
+++ js/app.js
@@ -1,46 +1,156 @@
 Vue.use(Framework7Vue);
 
-Vue.component('page-addServices',{
-  props:['caca'],
-  template:'<f7-page name="addServices"><f7-navbar><f7-nav-left><f7-link icon-material="keyboard_arrow_left"  back></f7-link></f7-nav-left><f7-nav-center>Add a service</f7-nav-center><f7-nav-right><f7-link icon="icon-gear"></f7-link></f7-nav-right></f7-navbar><f7-block>{{caca}}</f7-block></f7-page>'
-});
-
-Vue.component('page-settings',{
-  template:'<f7-page name="settings"><f7-navbar><f7-nav-left><f7-link icon-material="keyboard_arrow_left"  back></f7-link></f7-nav-left><f7-nav-center>Settings</f7-nav-center><f7-nav-right><f7-link icon="icon-gear"></f7-link></f7-nav-right></f7-navbar><f7-block></f7-block></f7-page>'
-});
+var $$ = Dom7;
 
 var app = new Vue({
   el: "#app",
   framework7: {
     root: '#app',
-    routes: [
-      {
-        path: '/add/',
-        component: 'page-addServices'
-      },
-      {
-        path: '/settings/',
-        component:'page-settings'
-      }
-    ]
+    routes: [],
+    material: true
   },
   data: {
-    caca:"pipi",
-    connected: undefined,
-    noAddress: false,
-    address: localStorage.lisaAddress
+    inputDatetime: '',
+    inputReminderText: '',
+    address: localStorage.lisaAddress,
+    musicStatus: {
+      musicPlaying: undefined,
+      musicTrack: undefined,
+      musicRepeat: undefined,
+      musicShuffle: undefined
+    },
+    weather: undefined,
+    socket: undefined,
+    settings: {
+        mail : {
+          enabled : undefined,
+          username : undefined,
+          password : undefined,
+          host : undefined
+        },
+        alarm : {
+          enabled : undefined,
+          music_onwake : undefined,
+          secondes:undefined,
+          minutes:undefined,
+          heures:undefined,
+          jour_mois:undefined,
+          mois:undefined,
+          jour_semaine:undefined
+        },
+        weather :{
+          weather_id : undefined,
+          cityID : undefined,
+          units : undefined
+        },
+        present : undefined,
+        appLanguage : undefined,
+        voiceLanguage: undefined,
+        sentences : {
+          wakeup : undefined
+        },
+        devices : [],
+        defaultAnswers: {},
+        reminders: []
+      }
   },
   methods: {
       connect: function () {
         localStorage.lisaAddress = this.address;
         socketCo();
       },
-      reconnect: function () {
-        socketCo();
+      next: function () {
+        this.socket.emit('music', 'next');
       },
-      changeAddress: function () {
-        this.noAddress = true;
-        localStorage.removeItem('lisaAddress');
+      previous: function () {
+        this.socket.emit('music', 'previous');
+      },
+      togglePlay: function () {
+        this.musicStatus.musicPlaying = ! this.musicStatus.musicPlaying;
+        this.socket.emit('music', 'togglePlay');
+      },
+      toggleShuffle: function () {
+        this.musicStatus.musicShuffle = !this.musicStatus.musicShuffle;
+        this.socket.emit('music', 'shuffle');
+      },
+      toggleRepeat: function () {
+        this.musicStatus.musicRepeat = !this.musicStatus.musicRepeat;
+        this.socket.emit('music', 'repeat');
+      },
+      stop: function () {
+        this.musicStatus.musicPlaying = false;
+        this.socket.emit('music', 'stop');
+      },
+      stopLisa: function () {
+        if (confirm('Do you want really want to shutdown the server ?')) {
+          this.socket.emit("cmd", "stop");
+        }
+      },
+      shutdown: function () {
+        if (confirm('Do you want really want to shutdown the server ?')) {
+          this.socket.emit('cmd',"shutdown");
+        }
+      },
+      refresh: function () {
+        this.socket.emit('refresh',"");
+      },
+      addReminder: function () {
+        this.settings.reminders.unshift({date: this.inputDatetime, text: this.inputReminderText});
+        this.inputDatetime = '';
+        this.inputReminderText = '';
+      },
+      deleteReminder: function (index) {
+        this.settings.reminders.splice(index,1);
       }
+  },
+  computed: {
+    areDevices: function () {
+      if (Object.keys(this.settings.devices).length > 0) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+    degreesUnits: function () {
+      if (this.settings.weather.units == "imperial") {
+        return "F";
+      } else if (this.settings.weather.units == "metric") {
+        return "C";
+      }
+      else {
+        return "K";
+      }
+    },
+    alarmTime: {
+      get: function () {
+        return this.settings.alarm.heures + ":" + this.settings.alarm.minutes;
+      },
+      set: function (value) {
+        var time = value.split(':');
+        this.settings.alarm.heures = time[0];
+        this.settings.alarm.minutes = time[1];
+      }
+    }
+  },
+  filters: {
+    moment: function (value) {
+      moment.locale(app.settings.appLanguage);
+      return moment.unix(value).format('lll');
+    },
+    round: function (value) {
+      return Math.round(value);
+    },
+    capitalize: function (value) {
+      return value.toUpperCase();
+    }
   }
+});
+
+// watchers
+app.$watch('settings',function () {
+      app.socket.emit('settings', app.settings);
+      app.socket.emit('refresh', "");
+  }, {
+  deep: true
 });
--- js/connect.js
+++ js/connect.js
@@ -1,27 +1,57 @@
-function socketCo() {
-
-  app.noAddress = false;
-  var socket = io.connect('http://' + localStorage.lisaAddress + ":8080");
-
-  socket.on('connect_error', function() {
-    console.log('Connect error !');
-    socket.close();
-    app.connected = false;
-  });
-
-  socket.on('connect', function functionName() {
-    console.log('Connecté!');
-    app.connected = true;
-  });
-
-  socket.on('donnees', function (data) {
-    app.$data.donnees = data;
-  });
-}
-
-
 if (localStorage.lisaAddress) {
   socketCo();
 } else {
-  app.noAddress = true;
+  app.$f7.loginScreen();
+}
+
+function socketCo() {
+  var socket = io.connect('http://' + localStorage.lisaAddress + ":8080");
+  app.socket = socket;
+
+// events
+  socket.on('connect_error', function() {
+    console.log('Connect error !');
+    socket.close();
+    app.$f7.closeModal();
+    app.$f7.loginScreen();
+    app.$f7.closeNotification('.notifications');
+    app.$f7.alert("<b>Error</b> : no connection ! Check your local network or the address of the host server.",["Lisa"]);
+  });
+
+  socket.on('connect', function () {
+    console.log('Connecté!');
+    app.$f7.closeModal();
+  });
+
+  socket.on('client', function (data) {
+    if (data == true) {
+      app.$f7.closeNotification('.notifications');
+      app.$f7.addNotification({message: "Another client is connected !"});
+    }
+  });
+
+  socket.on('musicPlaying', function (data) {
+    app.musicStatus.musicPlaying = data;
+  });
+
+  socket.on('musicTrack', function (data) {
+    app.musicStatus.musicTrack = data;
+  });
+
+  socket.on('musicRepeat', function (data) {
+    app.musicStatus.musicRepeat = data;
+  });
+
+  socket.on('musicShuffle', function (data) {
+    app.musicStatus.musicShuffle = data;
+  });
+
+  socket.on('settings', function (data) {
+    app.settings = data;
+  });
+
+  socket.on('weather', function (data) {
+    app.weather = data;
+  });
+
 }
